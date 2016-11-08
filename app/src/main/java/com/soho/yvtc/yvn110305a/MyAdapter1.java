001482150dffe8114157dfac815071147ed8e68d
pechentw <pechen.tw@gmail.com>
2016-11-08 19:54:20 +0800

    to 5th code add button

--- app/src/main/java/com/soho/yvtc/yvn110305a/MyAdapter1.java
+++ app/src/main/java/com/soho/yvtc/yvn110305a/MyAdapter1.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by yvtc on 2016/11/8.
@@ -37,12 +39,20 @@ public class MyAdapter1 extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from( context);
         View v = inflater.inflate(R.layout.myitem, null);
 
         TextView tv = (TextView) v.findViewById(R.id.textView);
         tv.setText(data[position]);
+
+        Button btn = (Button) v.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, data[position], Toast.LENGTH_SHORT).show();
+            }
+        });
         return v;
     }
 }
